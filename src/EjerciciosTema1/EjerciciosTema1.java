8bf24a3ca9b1062e88f3d513e219712a94af8138
Erpini <javipinilla77@gmail.com>
2021-12-31 09:06:49 +0100

    Se agrega el proyect

---
+++ src/EjerciciosTema1/EjerciciosTema1.java
@@ -0,0 +1,49 @@
+package EjerciciosTema1;
+
+public class EjerciciosTema1 {
+
+    public static void main(String[]args){
+
+        // Imprimir todos los tipos de datos, hacer variable e imprimirla
+
+        byte variable1 = 1;
+        short variable2 = 5;
+        int variable3 = 10;
+        long variable4 = 100;
+
+        // decimales
+        float variable5 = 6.5f;
+        double variable6 = 18.9d;
+
+        // boolean
+        boolean variable7 = true;
+        boolean variable8 = false;
+
+        // Texto
+        char variable9 = 'a'; // debajo de la interrogacion tengo la comilla sola
+        String variable10 = "Hola Mundo";
+
+        // ahora voy a imprimir cada una de las variables
+
+        System.out.println(variable1);
+        System.out.println(variable2);
+        System.out.println(variable3);
+        System.out.println(variable4);
+        System.out.println(variable5);
+        System.out.println(variable6);
+        System.out.println(variable7);
+        System.out.println(variable8);
+        System.out.println(variable9);
+        System.out.println(variable10);
+
+        // Finalizado el ejercicio.
+
+
+
+
+
+
+
+
+    }
+}
